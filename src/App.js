08324eb84f925474473e2c155509bcb0c48910e7
VuongTrinh <vuongtrinh.dev@gmail.com>
2021-03-06 14:06:11 +0700

    Initial configuration and finish MainLayout.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import componentQueries from 'react-component-queries';
+import MainLayout from 'components/Layout/MainLayout';
+import { BrowserRouter } from 'react-router-dom';
+
+import './styles/reduction.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <MainLayout></MainLayout>
+    </BrowserRouter>
   );
 }
 
-export default App;
+const query = ({ width }) => {
+  if (width < 575) {
+    return { breakpoint: 'xs' };
+  }
+
+  if (576 < width && width < 767) {
+    return { breakpoint: 'sm' };
+  }
+
+  if (768 < width && width < 991) {
+    return { breakpoint: 'md' };
+  }
+
+  if (992 < width && width < 1199) {
+    return { breakpoint: 'lg' };
+  }
+
+  if (width > 1200) {
+    return { breakpoint: 'xl' };
+  }
+
+  return { breakpoint: 'xs' };
+};
+
+export default componentQueries(query)(App);
---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Navbar, Nav, NavItem } from 'reactstrap';
+
+const Footer = () => {
+    return (
+        <Navbar>
+            <Nav navbar>
+                <NavItem>
+                    Copyright by Lucartist
+                </NavItem>
+            </Nav>
+        </Navbar>
+    );
+};
+
+export default Footer;
